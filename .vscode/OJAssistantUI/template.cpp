76fdf0fee0a1183f39a6a31c18acd8772b6a34a2
gzshawnliang <gzshawnliang@gmail.com>
2021-04-24 12:24:00 +0800

    template.cpp

--- .vscode/OJAssistantUI/template.cpp
+++ .vscode/OJAssistantUI/template.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 -------------------------------------------------------------------
 * @Name:           {Name}
 * @Author:         {Author}
@@ -6,18 +6,27 @@
 * @Url:            {Url}
 * @Description:    {Description}
 -------------------------------------------------------------------
-                                     /~\
-                                    |oo )
-                                    _\=/_
-                    ___        #   /  _  \
-                   / ()\        \\//|/.\|\\
-                 _|_____|_       \/  \_/  ||
-                | | === | |         |\ /| ||
-                |_|  O  |_|         \_ _/  #
-                 ||  O  ||          | | |
-                 ||__*__||          | | |
-                |~ \___/ ~|         []|[]
-                /=\ /=\ /=\         | | |
-________________[_]_[_]_[_]________/_]_[_\_________________________
+        _____
+        |_ _|
+   n    (O O)    n
+   H   _|\_/|_   H
+  nHnn/ \___/ \nnHn
+ <V VV /     \ VV V>
+  \__\/|     |\/__/
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("{fileNameNoExtension}.in", "r", stdin);
+    freopen("{fileNameNoExtension}.out", "w", stdout);
+#endif
+
+    return 0;
+}
