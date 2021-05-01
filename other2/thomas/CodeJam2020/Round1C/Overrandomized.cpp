83928521fea099c2f1e6130e817823153cf5c150
shawn <gzshawnliang@gmail.com>
2021-05-01 11:23:37 +0800

    codejam2020

---
+++ other2/thomas/CodeJam2020/Round1C/Overrandomized.cpp
@@ -0,0 +1,50 @@
+#ifndef LOCAL_DEBUG
+#define NDEBUG //ban assert when submit to online judge
+#endif
+
+#ifdef LOCAL_DEBUG
+#include <chrono>
+#endif
+
+#include <bits/stdc++.h>
+
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    std::cin.tie(nullptr);            \
+    std::cout.tie(nullptr)
+
+using namespace std;
+
+using ill = long long;
+
+class solution
+{
+  public:
+    void solve()
+    {
+        
+    }
+};
+
+signed main()
+{
+    FastIO;
+
+#ifdef LOCAL_DEBUG
+    freopen("Overrandomized.in", "r", stdin);
+    freopen("Overrandomized.out", "w", stdout);
+    auto startTime = chrono::high_resolution_clock::now();
+#endif
+
+    solution sln1;
+    sln1.solve();
+    cout.flush();
+
+#ifdef LOCAL_DEBUG
+    cerr << "Execution time: "
+         << chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - startTime).count()
+         << " ms\n";
+#endif
+
+    return 0;
+}
