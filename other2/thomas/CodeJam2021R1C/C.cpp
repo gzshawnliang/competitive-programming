66fb01c6e4a3efdcca1529e9e672b31b1a3fbbaf
shawn <gzshawnliang@gmail.com>
2021-05-10 11:25:57 +0800

    Round 1C 2021 - Code Jam 2021

---
+++ other2/thomas/CodeJam2021R1C/C.cpp
@@ -0,0 +1,53 @@
+#ifndef LOCAL_DEBUG
+#define NDEBUG //ban assert when submit to online judge
+#endif
+
+#ifdef LOCAL_DEBUG
+#include <chrono>
+#endif
+
+#include <bits/stdc++.h>
+
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    std::cin.tie(nullptr);            \
+    std::cout.tie(nullptr)
+
+using namespace std;
+
+using ill = long long;
+
+class solution
+{
+  public:
+    void solve()
+    {
+        bitset<10> a("1010");
+        bitset<10> b =(a << 1);
+        clog << a.to_string() << "\n";
+        clog << b.to_string() << "\n";
+    }
+};
+
+signed main()
+{
+    FastIO;
+
+#ifdef LOCAL_DEBUG
+    freopen("C.in", "r", stdin);
+    freopen("C.out", "w", stdout);
+    auto startTime = chrono::high_resolution_clock::now();
+#endif
+
+    solution sln1;
+    sln1.solve();
+    cout.flush();
+
+#ifdef LOCAL_DEBUG
+    cerr << "Execution time: "
+         << chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - startTime).count()
+         << " ms\n";
+#endif
+
+    return 0;
+}
