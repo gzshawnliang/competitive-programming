46da7c4a23cf994e9332cae5af614eb79a364ac1
shawn <gzshawnliang@gmail.com>
2021-11-29 16:23:27 +0800

    python

---
+++ Python/test.py
@@ -0,0 +1,10 @@
+import time;  # This is required to include time module.
+
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print (counter)
+print (miles)
+print (name)
+print ( "time:", time.asctime( time.localtime(time.time())))
