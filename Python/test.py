869a01fe9f0e617398e5115c4178a1674c24ae96
shawn <gzshawnliang@gmail.com>
2022-01-02 19:51:06 +0800

    No commit message

--- Python/test.py
+++ Python/test.py
@@ -8,3 +8,8 @@ print (counter)
 print (miles)
 print (name)
 print ( "time:", time.asctime( time.localtime(time.time())))
+
+sum = 0
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    sum = sum + x
+    print(x,":",sum)
