697c4953d396759a56b818527c3b58a48ca5ba46
shawn <gzshawnliang@gmail.com>
2022-02-23 18:23:17 +0800

    Cow Frisbee

--- usaco-practice/other/2022Jan_Silver_CowFrisbee/CowFrisbee.cpp
+++ usaco-practice/other/2022Jan_Silver_CowFrisbee/CowFrisbee.cpp
@@ -22,7 +22,27 @@ class solution
   public:
     void solve()
     {
-        
+        int n;
+        cin >> n;
+        vector<int> a(n+1);
+        stack<int> s;
+        for (int i = 1; i <= n; ++i)
+            cin >> a[i];
+        int ans = 0;
+        for (int i = 1; i <= n; ++i)
+        {
+            while (!s.empty() && a[i]>a[s.top()])
+            {
+                ans +=i-s.top()+1;
+                s.pop();
+            }
+            
+            if(!s.empty())
+                ans +=i-s.top()+1;
+            s.push(i);
+        }
+        cout << ans << '\n';
+
     }
 };
 
@@ -32,7 +52,7 @@ signed main()
 
 #ifdef LOCAL_DEBUG
     freopen("CowFrisbee.in", "r", stdin);
-    freopen("CowFrisbee.out", "w", stdout);
+    //freopen("CowFrisbee.out", "w", stdout);
     auto startTime = chrono::high_resolution_clock::now();
 #endif
 
